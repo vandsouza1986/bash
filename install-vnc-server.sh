3f150737075b50017367f62930dafca1a177dc95
vandsouza1986 <36281368+vandsouza1986@users.noreply.github.com>
2025-04-13 17:31:52 -0300

    Add files via upload

---
+++ install-vnc-server.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+
+export DEBIAN_FRONTEND=noninteractive
+
+sudo apt update
+sudo apt install -y lightdm
+sudo reboot
+sudo apt install -y x11vnc
+
+
+touch /lib/systemd/system/x11vnc.service
+echo <<EOF> /lib/systemd/system/x11vnc.service
+
+!Copy and paste these commands - change the password
+[Unit]
+Description=x11vnc service
+After=display-manager.service network.target syslog.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/x11vnc -forever -display :0 -auth guess -passwd password
+ExecStop=/usr/bin/killall x11vnc
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+
+
+systemctl daemon-reload
+systemctl enable x11vnc.service
+systemctl start x11vnc.service
+systemctl status x11vnc.service
