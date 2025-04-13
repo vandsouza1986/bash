1e5fbbd6d33e3e56b329cb0e4990f3f776ae3b18
vandsouza1986 <36281368+vandsouza1986@users.noreply.github.com>
2025-04-13 18:23:15 -0300

    Update install-vnc-server.sh

--- install-vnc-server.sh
+++ install-vnc-server.sh
@@ -1,27 +1,43 @@
-#!/bin/bash
-# Baseado no video de David Bomball
-# link https://www.youtube.com/watch?v=3K1hUwxxYek
-# 13/04/2025
+#!/usr/bin/env bash
+# Autor: Vanderson Souza
+# Data: 13/04/2025
+# Versao: 1.0
+# Roadmap -> validação sobre usuario root e verificar se diretorios/arquivos existem
+
+#Variaveis Global
+ATUALIZA="apt update"
+INSTALA="apt install -y"
+ARQUIVO="/lib/systemd/system/x11vnc.service"
+RECARREGA="systemctl daemon-reload"
+HABILITA="systemctl enable x11vnc.service"
+INICIA="systemctl start x11vnc.service"
+VERIFICA="systemctl status x11vnc.service"
+LOG_STATUS="/var/log/x11vnc.log"
+LOG_ERRO="/var/log/x11vnc.error"
+
+
+#Variaveis arquivo configuracao
+USUARIO="guess"
+SENHA=qweasd"
 
 export DEBIAN_FRONTEND=noninteractive
 
-sudo apt update
-sudo apt install -y lightdm
-sudo reboot
-sudo apt install -y x11vnc
+$ATUALIZA
+$INSTALA lightdm
+#sudo reboot
+$INSTALA x11vnc
 
 
-touch /lib/systemd/system/x11vnc.service
-cat <<EOF> /lib/systemd/system/x11vnc.service
+touch $ARQUIVO
+cat <<EOF> $ARQUIVO
 
-!Copy and paste these commands - change the password
 [Unit]
 Description=x11vnc service
 After=display-manager.service network.target syslog.target
 
 [Service]
 Type=simple
-ExecStart=/usr/bin/x11vnc -forever -display :0 -auth guess -passwd password
+ExecStart=/usr/bin/x11vnc -forever -display :0 -auth $USUARIO -passwd $SENHA
 ExecStop=/usr/bin/killall x11vnc
 Restart=on-failure
 
@@ -31,7 +47,9 @@ WantedBy=multi-user.target
 EOF
 
 
-systemctl daemon-reload
-systemctl enable x11vnc.service
-systemctl start x11vnc.service
-systemctl status x11vnc.service
+$RECARREGA *>> $LOG_STATUS
+$HABILITA *>> $LOG_STATUS
+$INICIA *>> $LOG_STATUS
+$VERIFICA *>> $LOG_STATUS
+
+echo -e "Recomenda-se reiniciar o computador"
